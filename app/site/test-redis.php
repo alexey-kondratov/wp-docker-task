b1c659e18abc9161a7202b3ac7b552b168ac99f3
Alexey Kondratov <alexey.kondratov@saritasa.com>
2018-05-03 15:46:49 +0700

    Debug and fix redis access error

--- app/site/test-redis.php
+++ app/site/test-redis.php
@@ -1,9 +1,11 @@
 <?php
 //simple counter to test sessions. should increment on each page reload.
+ini_set("display_errors", true);
 session_start();
 $count = isset($_SESSION['count']) ? $_SESSION['count'] : 1;
 
 echo $count;
 
 $_SESSION['count'] = ++$count;
+
 ?>
