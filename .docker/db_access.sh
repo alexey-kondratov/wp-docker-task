f3e93c424684da34f50b8d8179ef33a77651c972
Alexey Kondratov <alexey.kondratov@saritasa.com>
2018-05-03 17:36:26 +0700

    Fix bug script bug and irrelevant data

--- .docker/db_access.sh
+++ .docker/db_access.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+sleep 3
 mysql -e 'CREATE DATABASE `database-wordpress`;'
 mysql -e "CREATE USER 'user-wordpress'@'%' IDENTIFIED BY 'EHeGecnMJjgw39qy';"
 mysql -e "GRANT ALL ON *.* TO 'user-wordpress'@'%';"
